c1330a2041b40f8a714a613f6dd1478d4e9d3efd
Bhakin <{bhakinwork@hotmail.com}>
2020-08-17 11:22:30 +0700

    TEST1

---
+++ Bhakin_streamlit.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title('MY FIRST DEPLOYMENT OF STREAMLIT IN HEROKU')
+    st.subheader('Steamlit')
+
+if __name__ == '__main__':
+    main()
---
+++ Procfile
@@ -0,0 +1,2 @@
+web: sh setup.sh && streamlit run Bhakin_streamlit.py
+web: sh setup.sh && streamlit run app.py
